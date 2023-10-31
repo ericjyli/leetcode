4f91bac411473e64648478bd20ea8d5800eb55e2
Junyu Li <junyu.li@mail.utoronto.ca>
2023-10-30 20:45:14 -0400

    Finished 22 dp

--- 22.generate_parentheses/C++/22.cpp
+++ 22.generate_parentheses/C++/22.cpp
@@ -4,137 +4,66 @@
 
 // ((()))
 
-
 #include <string>
 #include <vector>
 #include <list>
 #include <map>
 #include <iostream>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
+
+set<string> insertOneParenthesis(string &original_str) {
+    set<string> res_set;
+
+    for (int i = 0; i <= original_str.length(); ++i) {
+        if (original_str[i] != '(') {
+            string temp_str = original_str;
+            temp_str.insert(i, "(");
+            for (int j = i + 1; j <= temp_str.length(); ++j) {
+                if (temp_str[j] != ')') {
+                    temp_str.insert(j, ")");
+                    res_set.insert(temp_str);
+                    temp_str.erase(j, 1);
+                }
+
+            }
+        }
+
+    }
+
+    return res_set;
+}
+
 // 1 <= n <= 8
 vector<string> generateParenthesis(int n) {
-    vector<string> res;
-    if (n <= 0) return res;
-    pair<char, char> parenthesis('(', ')');
 
+    vector<vector<string>> dp(n + 1);
+    dp[0] = {};
+    dp[1] = {"()"};
+    if (n == 1) return dp[n];
+    dp[2] = {"()()", "(())"};
+    if (n == 2) return dp[n];
 
-
-    // n = 3
-    // ()()()   3
-    // (())()   1
-    // ()(())   1
-    // ((()))   0
-
-    // n = 4
-    //
-    // ()()()() 4
-    //
-    // (())()() 2
-    // ()(())() 2
-    // ()()(()) 2
-    //
-    // ((()))() 1
-    // ()((())) 1
-    //
-    // (((()))) 0
-
-    // n = 5
-    // ()()()()() 5
-
-    // (())()()() 3
-    // ()(())()() 3
-    // ()()(())() 3
-    // ()()()(()) 3
-
-    // ((()))()() 2
-    // ()((()))() 2
-    // ()()((())) 2
-
-    // (((())))() 1
-    // ()(((()))) 1
-
-    // ((((())))) 0
-
-
-    // number of single (not nested) parenthesises
-    // the index of them
-
-
-
-    int single_num = n;
-    vector<bool> single(n);
-    for (int i = 0; i < n; ++i) {
-        single[i] = true;
-    }
-
-    while (true) {
-        // generate the positions of the single parenthesises
-        bool valid = true;
-        string temp_str;
-        temp_str.resize(n * 2);
-
-        // fill in the single parenthesises first
-        for (int i = 0; i < n; ++i) {
-            if (single[i]) {
-                temp_str[i * 2] = '(';
-                temp_str[i * 2 + 1] = ')';
+    // i is the number of parentheses
+    for (int i = 3; i <= n; ++i) {
+        set<string> temp;
+        for (int j = 0; j < dp[i - 1].size(); ++j) {
+            string temp_str = dp[i - 1][j];
+           
+            // add a parentheses for this temp string
+            set<string> generated_strs = insertOneParenthesis(temp_str);    
+            for (auto it = generated_strs.begin(); it != generated_strs.end(); ++it) {
+                temp.insert(*it);
             }
         }
-
-        for (int i = 0; i < n; ++i) {
-            if (!single[i]) {
-                int start_idx = i;
-                while (!single[i] && i < n) {
-                    i++;
-                }
-                i--;
-                if (start_idx == i) {
-                    valid = false;
-                    break;
-                }
-                else {
-                    int j = start_idx * 2;
-                    int num = i - j + 1;
-                    for (int k = 0; k < num; ++k) {
-                        temp_str[j] = '(';
-                        j++;
-                    }
-                    for (int k = 0; k < num; ++k) {
-                        temp_str[j] = ')';
-                        j++;
-                    }
-                }
-            }
-        }
-
-
-        if (!valid) {
-            continue;
-        }
-        else {
-            res.push_back(temp_str);
-
-        }
-
-
-        // iterate by changing single_num and vector<bool> single
-        if (single[n - 1] == false) {
-            single_num--;
-        }
-
-        else if (single_num == n) {
-            single_num = single_num - 2;
-        }
-
-        // not changing the number of single nums
-
-
-
+        dp[i].insert(dp[i].end(), temp.begin(), temp.end());
     }
 
-    return res;
+
+    return dp[n];
 }
 
 
