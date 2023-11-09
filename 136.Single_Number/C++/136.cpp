19119e24138322b6bcd4adb365a922ad773bfebe
Junyu Li <junyu.li@mail.utoronto.ca>
2023-11-09 10:11:41 -0500

    Finished 136

---
+++ 136.Single_Number/C++/136.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int ans = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        ans ^= nums[i];
+    }
+
+    return ans;
+}
