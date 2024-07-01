838adb016426a88aa3c68e8746f9d259de68d270
Junyu Li <junyu.li@mail.utoronto.ca>
2024-07-01 19:56:14 -0400

    Finished 1550

---
+++ 1550.Three_Consecutive_Odds/C++/1550.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+bool threeConsecutiveOdds(vector<int>& arr) {
+    int count = 0;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        if (arr[i] % 2 == 1) {
+            count ++;
+            if (count == 3) {
+                return true;
+            }
+        }
+        else {
+            count = 0;
+        }
+    }
+
+    return false;
+}
